efdb42099252ecf4d08495af3995ebdb72c35390
Debbie Yoro <d.anne@live.com>
2014-01-27 21:26:48 -0800

    added two pages and it’s route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
+  get "event" => "pages#event"
+  get "futureevents" => "pages#futureevents"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
